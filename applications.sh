526688f377bed45a258a4169c899a8adc0a72cc4
Luciano Pulido <lucianonicolaspulido@gmail.com>
2026-01-29 16:12:13 -0300

    Add applications auto-install

---
+++ applications.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+sudo snap install discord
+sudo apt install vlc
+sudo snap install dbeaver-ce --classic
+sudo apt install virt-manager
+sudo snap install --classic code
+flatpak install flathub com.obsproject.Studio
+
+git config --global user.email "lucianonicolaspulido@gmail.com"
+git config --global user.name "Luciano Pulido"
+
+# Instalar GitHub CLI
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) &&
+  sudo mkdir -p -m 755 /etc/apt/keyrings &&
+  out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg &&
+  cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null &&
+  sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg &&
+  sudo mkdir -p -m 755 /etc/apt/sources.list.d &&
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+  sudo apt update &&
+  sudo apt install gh -y
+
+sudo apt update
+sudo apt install gh
+
+gh auth login
+
+# Instalar docker
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+wget -O docker-desktop.deb "https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64"
+sudo apt-get update
+sudo apt install ./docker-desktop-amd64.deb
+
+systemctl --user enable docker-desktop
